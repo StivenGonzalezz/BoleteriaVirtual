b6575810ac419d4c8cdeb2118a95cfcc677acf3b
StivenGonzalezz <grstiven1004@gmail.com>
2024-05-13 19:34:59 -0500

    borrado de carpeta 'clases'

--- src/main/java/clases/Docente.java
+++
@@ -1,8 +0,0 @@
-package clases;
-
-public class Docente extends Persona{
-
-    public Docente(String nombre, String apellidos, String documento) {
-        super(nombre, apellidos, documento);
-    }
-}

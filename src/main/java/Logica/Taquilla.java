a7cfe8b097c810160efdd10e16340f5ea453f1ee
Carlos Colorado <117797274+CarlosSantiagoColorado@users.noreply.github.com>
2024-05-25 22:30:41 -0500

    cierre de taquilla por medio de la hora
    solo permitirá la entrada a la hora de apertura(toca quemarla ) y una hora antes del evento, si genera algun tipo de error, comentar todo ese bloque y descomentar el bloque original :)

--- src/main/java/Logica/Taquilla.java
+++ src/main/java/Logica/Taquilla.java
@@ -1,5 +1,6 @@
 package Logica;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,9 +24,88 @@ public class Taquilla {
         String nombreEventoBus = scanner.nextLine();
         int i = 0;
         int total = 0;
+        LocalTime horaResta;
+        //Hora de apertura para el evento (quemada por el momento)
+        LocalTime horaAper= LocalTime.of(22,59);
+        LocalTime horaActual = LocalTime.now();
         ArrayList<Boleatas> boletosVendidos = new ArrayList<>();
         while (i < baseDatosEventos.size()) {
             Evento evento = baseDatosEventos.get(i);
+            //RESTA una hora a la hora del evento para validar hasta que hora se puede acceder al evento
+            horaResta = evento.getHora().minusHours(1);
+            if (horaActual.isAfter(horaAper) && horaActual.isBefore(horaResta)) {
+                System.out.println("Hora de entrada permitida");
+                System.out.println("Hora: " + horaActual.getHour() + ":" + horaActual.getMinute() + " PM");
+                if (evento.getNombre().equalsIgnoreCase(nombreEventoBus)) {
+                    System.out.print("Nombre de usuario:");
+                    nombreUser = scanner.nextLine();
+                    System.out.print("Documento de usuario:");
+                    documentoeUser = scanner.nextLine();
+                    System.out.print("Contraseña de usuario:");
+                    contrasenaUser = scanner.nextLine();
+
+                    boolean usuarioExistente = false;
+                    for (Usuario usuario : baseDatosUsuarios) {
+                        if (usuario.getNombre().equals(nombreUser) && usuario.getContrasena().equals(contrasenaUser) && usuario.getDocumento().equals(documentoeUser)) {
+                            apellidosUser = usuario.getApellidos();
+                            CorreoUser = usuario.getCorreo();
+                            usuarioExistente = true;
+                            break;
+                        }
+                    }
+
+                    if (!usuarioExistente) {
+                        System.out.println("Usuario no existe");
+                    } else if (usuarioExistente) {
+                        int cobre = evento.getPrecioCobre();
+                        int plata = evento.getPrecioPlata();
+                        int oro = evento.getPrecioOro();
+                        System.out.println("Bienvenido a la venta de boletos.");
+                        total = 0;
+                        int opcion = -1;
+                        int cantidad = 0;
+
+                        while (opcion != 0) {
+                            System.out.println("¿Qué tipo de boleto desea comprar?");
+                            System.out.println("Cobre:" + evento.getCobreDispo() + ", Plata:" + evento.getPlataDispo() + ", Oro:" + evento.getOroDispo());
+                            System.out.println("1. Cobre");
+                            System.out.println("2. Plata");
+                            System.out.println("3. Oro");
+                            System.out.println("0. Salir");
+                            opcion = scanner.nextInt();
+                            if (opcion != 0) {
+                                System.out.println("¿Cuántos boletos desea comprar?");
+                                cantidad = scanner.nextInt();
+                            }
+                            switch (opcion) {
+                                case 0:
+                                    break;
+                                case 1:
+                                    total += venderCobre(cantidad, cobre, evento, boletosVendidos);
+                                    break;
+                                case 2:
+                                    total += venderPlata(cantidad, plata, evento, boletosVendidos);
+                                    break;
+                                case 3:
+                                    total += venderOro(cantidad, oro, evento, boletosVendidos);
+                                    break;
+                                default:
+                                    System.out.println("Opción no válida");
+                                    break;
+                            }
+                        }
+                        archivos.escribirArchivoEvents(baseDatosEventos); // Guardar los cambios
+                        archivos.escribirArchivoBoletas(boletosVendidos); // Guardar los boletos vendidos
+                        break;
+                    }
+                }
+                i++;
+            }  else {
+            System.out.println("Taquilla cerrada");
+        }
+        i++;
+        }
+            /**
             if (evento.getNombre().equalsIgnoreCase(nombreEventoBus)) {
                 System.out.print("Nombre de usuario:");
                 nombreUser = scanner.nextLine();
@@ -89,9 +169,10 @@ public class Taquilla {
                     break;
                 }
             }
-            i++;
-        }
-        return total;
+            i++;**/
+          return total;
+    }
+        //return total;
     }
 
     private int venderCobre(int cantidad, int cobre, Evento evento, ArrayList<Boleatas> boletosVendidos) {
